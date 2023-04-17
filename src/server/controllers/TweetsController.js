252e1e89037241be2555775502ebda0ae160e553
jingzhou Tao <jingzhoutao@jingzhoudeMacBook-Air.local>
2023-04-17 15:24:05 -0700

    update tweets

---
+++ src/server/controllers/TweetsController.js
@@ -0,0 +1,89 @@
+const Tweets = require("../models.js/Tweets");
+
+const getTweets = (req,res) => {
+    Tweets.find()
+    .then((response) => {
+        res.json({
+            response
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: error
+        })
+    })
+}
+
+const getTweet = (req,res) => {
+    const tweetId = req.body.tweetId;
+    Tweets.findById(tweetId)
+    .then((response) => {
+        res.json(
+            {response}
+        )
+    })
+    .catch((e) => res.json({
+        message: e
+    }) )
+}
+
+const createTweet = (req, res) => {
+    const Tweet = new Tweets({
+        content: req.body.content,
+        imageUrl: req.body.imageUrl
+    })
+    Tweet.save()
+    .then((response) => {
+        res.json({
+            message: "Tweet created successfully!"
+        })
+    })
+    .catch((e) => {
+        res.json({
+            message: "An error occurred!"
+        })
+    })
+}
+
+const updateTweet = (req,res) => {
+    let tweetId = req.body.tweetId;
+
+    const updatedTweetData = {
+        content: req.body.content,
+        imageUrl:req.body.imageUrl
+    }
+
+    Tweets.findByIdAndUpdate(tweetId, {$set: updatedTweetData})
+    .then((resonse) => {
+        res.json({
+            message: "Tweet updated!"
+        })
+    })
+    .catch((e) => {
+        res.json({
+            message: e
+        })
+    })
+}
+
+const deleteTweet = (req, res) => {
+    let tweetId = req.body.tweetId;
+    Tweets.findByIdAndRemove(tweetId)
+    .then((response) => {
+        res.json({
+            message: 'Tweet deleted successfully!'
+        })
+    })
+    .catch(e => {
+        res.json({
+            message: e
+        })
+    })
+}
+module.exports = {
+    getTweets,
+    getTweet,
+    updateTweet,
+    createTweet,
+    deleteTweet
+} 
